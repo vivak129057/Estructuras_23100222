5f6bdd8cde7ccdcff2c3cc06015297535419b2db
vivak129057 <kv2304422e@gmail.com>
2025-10-13 15:13:10 -0500

    Carpeta ejercicio palindromo

---
+++ ejercicio_1_palindromo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindromo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> pila = new Stack<char>();
+            string strPalabra;
+            string CadenaInv = "";
+
+            Console.WriteLine("Digite la palabra");
+            strPalabra = Console.ReadLine();
+
+            strPalabra= strPalabra.ToUpper();
+
+            for (int i = 0; i < strPalabra.Length; i++)
+            {
+                pila.Push(strPalabra[i]);
+            }
+
+            while (pila.Count > 0)
+            {
+                char elemento = pila.Pop();
+                CadenaInv += elemento;
+            }
+
+            if (strPalabra == CadenaInv)
+            {
+                Console.WriteLine("Es palindromo");
+            }
+            else
+            {
+                Console.WriteLine($"No es palindromo: {CadenaInv} y {strPalabra}");
+            }
+
+
+
+        }
+    }
+}
