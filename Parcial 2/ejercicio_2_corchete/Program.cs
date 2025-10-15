a2b13478a3c6cc46979d1d13fa65809f36d2604d
vivak129057 <kv2304422e@gmail.com>
2025-10-14 21:20:16 -0500

    codigo actualizado

--- Parcial 2/ejercicio_2_corchete/Program.cs
+++ Parcial 2/ejercicio_2_corchete/Program.cs
@@ -10,6 +10,48 @@ namespace corchete
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ingresa una operación aritmética:");
+            string expresion = Console.ReadLine();
+
+            if (ValidarBalance(expresion))
+                Console.WriteLine("La expresión está bien escrita.");
+            else
+                Console.WriteLine("La expresión NO está bien escrita.");
+
+            Console.ReadKey();
         }
+        
+         static bool ValidarBalance(string expresion)
+        {
+            Stack<char> pila = new Stack<char>();
+
+            foreach (char C in expresion)
+            {
+                
+                if ( C == '(' || C == '[' || C == '{')
+                {
+                    pila.Push(C);
+                }
+                else if (C == ')' || C  == ']' || C == '}')
+                {
+                    if (pila.Count == 0)
+                        return false;
+
+                    char tope = pila.Pop();
+
+                    if (!Coinciden(tope, C))
+                        return false;
+                }
+            }
+            return pila.Count == 0;
+        }
+
+        static bool Coinciden(char apertura, char cierre)
+        {
+            return (apertura == '(' && cierre == ')') ||
+                    (apertura == '[' && cierre == ']') ||
+                    (apertura == '{' && cierre == '}');
+        }
+
     }
 }
